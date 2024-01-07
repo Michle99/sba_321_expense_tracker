3c8ce57682e7e3b257c543555e9ed673345aff68
Michle99 <sammiyonyx@gmail.com>
2024-01-07 16:21:01 -0500

    add expenses functionalities

--- src/app/expense.service.ts
+++ src/app/expense.service.ts
@@ -5,5 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class ExpenseService {
 
+  private expenses: any[] = [];
+
+  getExpenses() {
+    return this.expenses;
+  }
+
+  addExpenses(expense: any) {
+    this.expenses.push(expense);
+  }
+
+  deleteExpense(index: number) {
+    this.expenses.splice(index, 1);
+  }
+
   constructor() { }
 }
