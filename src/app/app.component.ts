96c67cc57a9984ba5ffdef48f10872beeb1e1677
Michle99 <sammiyonyx@gmail.com>
2024-01-07 20:50:41 -0500

    updated `header` css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
@@ -10,7 +12,9 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     CommonModule, 
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
