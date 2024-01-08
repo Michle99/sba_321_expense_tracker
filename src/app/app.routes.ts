f904931359164c7dd66901db0707318fb1ab24f6
Michle99 <sammiyonyx@gmail.com>
2024-01-07 20:25:04 -0500

    add app component routing to `app.routing.ts`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'expenses', component: ExpensesComponent },
+    { path: 'about', component: AboutComponent },
+];
